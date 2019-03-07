946599f40be95453e2627178547296ff51f5476a
AllanDickerman <allan@vt.edu>
2019-03-07 16:43:19 +0100

    More detail added to README.md and options streamlined.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # p3_read_mapping
 Use kma to ap fastq reads to specified set of genes, for MLST etc.
 
+Requirements:
+	kma is on path
+	kma index is available and specified as --kma_index option
+	one single-end or paired-end fastq read library is specified as --se or --pe option
+	output directory is specified as --output_directory option, will be created if needed
+
+Output directory holds these files:
+	prefix.res: results telling which allele for each locus is matched best
+	prefix.aln: the alignment of the read assembly against best-hit alleles
+	prefix.fsa: sequence of read assembly for each locus
 
--- scripts/p3-read-mapping.py
+++ scripts/p3-read-mapping.py
@@ -9,13 +9,13 @@ import subprocess
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='A script to map fastq reads to a set of genes of interest, e.g. MLST alleles.',
-            usage='usage: p3-read-mapping.py --pe read1.fq:read2.fq --se reads.fq --sequence_set seq.fasta --kma_index name')
+            usage='usage: p3-read-mapping.py --pe read1.fq:read2.fq --se reads.fq --kma_index name')
 
     parser.add_argument('--pe', required=False, help='Paired-end fastq files (read1.fq:read2.fq)')
     parser.add_argument('--se', required=False, help='Single-end fastq file')
     parser.add_argument('--output_directory', required=True, help='Output directory')
-    parser.add_argument('--sequence_set_fasta', required=False, help='alternate allele sequences (fasta)')
-    parser.add_argument('--sequence_kma_index', required=False, help='indexed allele sequences for kma')
+    #parser.add_argument('--sequence_set_fasta', required=False, help='alternate allele sequences (fasta)')
+    parser.add_argument('--kma_index', required=False, help='indexed allele sequences for kma')
     parser.add_argument('--kma_options', default="-and")
 
     args = parser.parse_args()
@@ -33,10 +33,10 @@ if __name__ == '__main__':
         m = re.match(r"\w+", os.path.basename(file1))
         if m:
             filebase = m.group(0)
-    filebase += "_vs_"+os.path.basename(args.sequence_kma_index).replace(r"\..*", "")
+    filebase += "_vs_"+os.path.basename(args.kma_index).replace(r"\..*", "")
     if not os.path.exists(args.output_directory):
         os.mkdir(args.output_directory)
-    command = "kma -t_db "+args.sequence_kma_index
+    command = "kma -t_db "+args.kma_index
     command += " -o "+os.path.join(args.output_directory, filebase)
     if args.se:
         command += " -i "+args.se
